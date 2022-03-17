672bb25e33a62891d153a7a9f969b14e39ca1ea2
Marian Perez <marianoperezsu@gmail.com>
2022-03-16 23:52:45 -0300

    avances

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ const remeraAzul = new Prendas("remera azul", "manga corta", "xl", "remeras", 25
 const remeraRoja = new Prendas("remera roja", "manga larga", "m", "remeras", 1800, 35)
 const jeanNegro = new Prendas("jean negro", "al cuerpo", "40", "jeans", 4000, 33);
 const jeanCeleste = new Prendas("jean celeste", "holgado", "42", "jeans", 6000, 0);
-const pantalonGamuza = new Prendas("pantalón de gamuza", "3/4", "38 y 1/2", "pantalones", 1200, 50);
-const pantalonLargo = new Prendas("pantalón largo", "pantalon", "40", "pantalones", 8000, 81);
+const pantalonGamuza = new Prendas("pantalon de gamuza", "3/4", "38 y 1/2", "pantalones", 1200, 50);
+const pantalonLargo = new Prendas("pantalon largo", "pantalon", "40", "pantalones", 8000, 81);
+const bermuda = new Prendas("bermuda", "pantalon corto", "40", "pantalones", 3500, 25)
 const poleraBeige = new Prendas("polera beige", "manga larga", "xl", "polera", 7500, 40);
+
 
 const stock = [
     remeraGris,
@@ -31,6 +33,7 @@ const stock = [
     jeanCeleste,
     pantalonGamuza,
     pantalonLargo,
+    bermuda,
     poleraBeige
 ];
 
@@ -71,6 +74,9 @@ const botonCompra = (compra, cantidad) => {
                     precio: precioParcial
                 });
             }
+        } else {
+            alert(`Solo contamos con ${compra.cantidad} prendas de este producto`);
+
         }
 
     } else {
@@ -107,15 +113,13 @@ const botonReset = () => {
     carrito.splice(0, carrito.length)
 }
 
-const descuento = () => {
-    let codigo = prompt("decinos tu codigo de descuento")
-    codigo = codigo.toLowerCase();
+const descuento = (codigo) => {
     if (codigo === "remera") {
         total = subtotal() - porcentaje10(subtotal())
     } else if (codigo === "jeans") {
         total = subtotal() - porcentaje25(subtotal())
     } else {
-        alert("el codigo de descuento es invalido")
+        total = parseInt(subtotal());
     }
 };
 
@@ -155,11 +159,11 @@ let cantidad = document.querySelectorAll(".cantidad");
 let precio = document.querySelectorAll(".precio");
 
 
-body.style.margin = "0";
-body.style.padding = "0";
-divPadre.style.display = "flex";
-divPadre.style.flexWrap = "wrap";
-divPadre.style.justifyContent = "space-around";
+// body.style.margin = "0";
+// body.style.padding = "0";
+// divPadre.style.display = "flex";
+// divPadre.style.flexWrap = "wrap";
+// divPadre.style.justifyContent = "space-around";
 
 
 // const productoComprado = carrito.map ((el) =>{
@@ -180,45 +184,102 @@ divPadre.style.justifyContent = "space-around";
 
 
 
-for (let el of divHijo){
-    el.style.margin = "2rem 4rem"
-    el.style.textAlign = "center"
-    el.style.fontFamily = "sans-serif"
-}
+// for (let el of divHijo) {
+//     el.style.margin = "2rem 4rem"
+//     el.style.textAlign = "center"
+//     el.style.fontFamily = "sans-serif"
+// }
 
 
-for (let el of boton){
-    el.style.backgroundColor = "#FF861C";
-    el.style.color = "white";
-    el.style.height = "3rem";
-    el.style.borderRadius = "30px";
-    el.style.border = "none";
-    el.style.fontSize = "large";
-    el.style.fontweigh = "bold";
-    el.style.boxShadow = "-2px 4px 10px 4px rgba(0,0,0,0.19);"
-}
+// for (let el of boton) {
+//     el.style.backgroundColor = "#FF861C";
+//     el.style.color = "white";
+//     el.style.height = "3rem";
+//     el.style.borderRadius = "30px";
+//     el.style.border = "none";
+//     el.style.fontSize = "large";
+//     el.style.fontweigh = "bold";
+//     el.style.boxShadow = "-2px 4px 10px 4px rgba(0,0,0,0.19);"
+// }
 
 
 let consulta = (prompt("desea realizar una compra")).toLowerCase();
 
-while (consulta === "si"){
+while (consulta === "si") {
     let prenda = (prompt("que prenda desea comprar")).toLowerCase();
-    let cantidadPrenda = (prompt("Cuantas prendas desea comprar")).toLowerCase()
-        for (let el of stock){
-        if (el.nombre === prenda){
-    botonCompra(el, cantidadPrenda);
+    let verdadero = stock.some((el) => el.nombre === prenda);
+    let cantidadPrenda = parseInt(prompt("Cuantas prendas desea comprar"));
+    if (verdadero === true) {
+        for (let el of stock) {
+            if (el.nombre === prenda) {
+                if (el.cantidad > cantidadPrenda) {
+                    botonCompra(el, cantidadPrenda);
 
-     carrito.map((el) => {alert(`agregaste ${el.producto} a tu carrito`)})
+                    alert(`agregaste ${el.nombre} a tu carrito`)
+                } else {
+                    alert(`solo contamos con ${el.cantidad} prendas de este tipo`)
+                }
+            }
+        }
+    } else {
+        alert("No contamos con ese producto")
     }
-}
-consulta = prompt("desea realizar otra compra")
+    consulta = prompt("desea realizar otra compra")
 };
 
-let consulta2 = (prompt("Querés conocer saber cuantos productos estas llevando carrito")).toLowerCase();
+let consulta2 = (prompt("Querés saber cuantos productos estas llevando carrito")).toLowerCase();
 
-if (consulta2 === "si"){
-    
-   let cantidad = carrito.reduce((acc, el) => acc + el.cantidades, 0)
-   alert (`Llevás en tu carrito ${cantidad} productos con un total de ${total()}`)
+if (consulta2 === "si") {
 
+    let cantidad = carrito.reduce((acc, el) => acc + parseInt(el.cantidades), 0);
+    alert(`Llevás en tu carrito ${cantidad} productos con un total de ${subtotal()}$`);
+
+}
+
+let consulta4 = (prompt(`Desea eliminar algún producto de su carrito`)).toLowerCase();
+
+while (consulta4 === "si") {
+
+    let prenda = (prompt("Qué prenda desea eliminar")).toLowerCase();
+    let verdadero = carrito.some((el) => el.producto === prenda);
+
+    let cantidadPrenda = parseInt(prompt("Cuanta cantidad"));
+    let verdadero2 = carrito.some((el) => el.cantidades >= cantidadPrenda);
+    if (verdadero === true) {
+        if (verdadero2 === true) {
+            for (let el of stock) {
+                if (prenda === el.nombre && el.cantidades >= cantidadPrenda) {
+
+                    botonQuitarCompra(el, cantidadPrenda);
+                    alert(`sacaste ${cantidadPrenda} ${prenda} de tu carrito`)
+
+                    let cantidad = carrito.reduce((acc, el) => acc + parseInt(el.cantidades), 0);
+                    alert(`Ahora llevás en tu carrito ${cantidad} productos con un total de ${subtotal()}$`);
+
+                }
+            }
+        } else {
+            alert("No contas con esa cantidad de producto en tu carrito")
+        }
+    } else {
+        alert("Ese producto no se encuentra en el carrito")
+    }
+    consulta4 = (prompt("Desea eliminar otro producto")).toLowerCase()
+}
+
+let consulta3 = (prompt("queres aplicar algún descuento")).toLowerCase();
+
+if (consulta3 === "si") {
+
+    let codigo = (prompt("Decinos tu código de descuento")).toLowerCase();
+
+    descuento(codigo);
+
+
+    if (total != subtotal()) {
+        alert(`Tu monto anterior era de ${subtotal()}$`)
+        alert(`Tu monto con el descuento es del ${total}$`)
+    } else {
+        alert(`El codigo no es válido`)
+    }
 }
