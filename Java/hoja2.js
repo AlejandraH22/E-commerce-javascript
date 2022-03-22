526314b75dcbb4dd9e92f1c0ff6d2abea2052690
Marian Perez <marianoperezsu@gmail.com>
2022-03-22 20:26:16 -0300

    avances

---
+++ Java/hoja2.js
@@ -0,0 +1,38 @@
+class Prendas {
+    constructor(nombre, tipo, talle, categoria, precio, cantidad) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.talle = talle;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        if (this.cantidad > 0) {
+            this.stock = "si";
+        } else {
+            this.stock = "no"
+        }
+    }
+}
+
+const remeraGris = new Prendas("remera gris", "manga corta", "xxl", "remeras", 1500, 25);
+const remeraAzul = new Prendas("remera azul", "manga corta", "xl", "remeras", 2500, 42);
+const remeraRoja = new Prendas("remera roja", "manga larga", "m", "remeras", 1800, 35)
+const jeanNegro = new Prendas("jean negro", "al cuerpo", "40", "jeans", 4000, 33);
+const jeanCeleste = new Prendas("jean celeste", "holgado", "42", "jeans", 6000, 0);
+const pantalonGamuza = new Prendas("pantalon de gamuza", "3/4", "38 y 1/2", "pantalones", 1200, 50);
+const pantalonLargo = new Prendas("pantalon largo", "pantalon", "40", "pantalones", 8000, 81);
+const bermuda = new Prendas("bermuda", "pantalon corto", "40", "pantalones", 3500, 25)
+const poleraBeige = new Prendas("polera beige", "manga larga", "xl", "polera", 7500, 40);
+
+
+const stock = [
+    remeraGris,
+    remeraAzul,
+    remeraRoja,
+    jeanNegro,
+    jeanCeleste,
+    pantalonGamuza,
+    pantalonLargo,
+    bermuda,
+    poleraBeige
+];
--- Java/index.js
+++ Java/index.js
@@ -1,41 +1,3 @@
-class Prendas {
-    constructor(nombre, tipo, talle, categoria, precio, cantidad) {
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.talle = talle;
-        this.categoria = categoria;
-        this.precio = precio;
-        this.cantidad = cantidad;
-        if (this.cantidad > 0) {
-            this.stock = "si";
-        } else {
-            this.stock = "no"
-        }
-    }
-}
-
-const remeraGris = new Prendas("remera gris", "manga corta", "xxl", "remeras", 1500, 25);
-const remeraAzul = new Prendas("remera azul", "manga corta", "xl", "remeras", 2500, 42);
-const remeraRoja = new Prendas("remera roja", "manga larga", "m", "remeras", 1800, 35)
-const jeanNegro = new Prendas("jean negro", "al cuerpo", "40", "jeans", 4000, 33);
-const jeanCeleste = new Prendas("jean celeste", "holgado", "42", "jeans", 6000, 0);
-const pantalonGamuza = new Prendas("pantalon de gamuza", "3/4", "38 y 1/2", "pantalones", 1200, 50);
-const pantalonLargo = new Prendas("pantalon largo", "pantalon", "40", "pantalones", 8000, 81);
-const bermuda = new Prendas("bermuda", "pantalon corto", "40", "pantalones", 3500, 25)
-const poleraBeige = new Prendas("polera beige", "manga larga", "xl", "polera", 7500, 40);
-
-
-const stock = [
-    remeraGris,
-    remeraAzul,
-    remeraRoja,
-    jeanNegro,
-    jeanCeleste,
-    pantalonGamuza,
-    pantalonLargo,
-    bermuda,
-    poleraBeige
-];
 
 
 
@@ -47,7 +9,9 @@ let total = subtotal;
 
 // Funciones
 
-
+const guardarStorage = (clave, valor) =>{
+    localStorage.setItem(clave, valor)
+}
 const basePorcentaje = (n) => {
     return (m) => (m * n) / 100
 }
@@ -176,7 +140,6 @@ let linkCargaProducto = document.getElementById("linkCargaProducto");
 
 for (let i = 0; i < stock.length; i++) {
 
-    
     botonCarrito[i].addEventListener("click", agregarCarrito);
     function agregarCarrito() {
         if (stock[i].cantidad >= 1 && stock[i].cantidad >= botonCantidad[i].value) {
@@ -185,8 +148,8 @@ for (let i = 0; i < stock.length; i++) {
                 final.length = 0;
                 for (let el of carrito) {
                     final.push(`<div class="d-flex justify-content-around recuadro"> <p>${el.producto}</p> <p>${el.cantidades}</p> <p>${el.precio}</p> </div>`);
-                    final.join();
-                    seccionPrecio[0].innerHTML = `${final} ${subtotalPantalla.innerHTML = `<div class="recuadro d-flex justify-content-end"> Subtotal = ${subtotal()}`} </div> `;
+
+                    seccionPrecio[0].innerHTML = `${final.join("")} ${subtotalPantalla.innerHTML = `<div class="recuadro d-flex justify-content-end"> Subtotal = ${subtotal()}`} </div> `;
 
                 }
             }
@@ -262,7 +225,6 @@ cargarProducto.onclick = () => {
             let precio = DatosCarga[4].value;
             let cantidad = DatosCarga[5].value;
             let imagen = DatosCarga [6].value;
-            let nombre2 = "producto" + DatosCarga[0].value;
         
         
 
@@ -281,6 +243,10 @@ cargarProducto.onclick = () => {
    </div>`
 
    padreTarjeta.append(tarjetanueva);
+
+stock.push(nombre2);
+        let nombre2Storage = JSON.stringify(nombre2);
+        guardarStorage("ProductosNuevos", nombre2Storage)
 
     }
 
